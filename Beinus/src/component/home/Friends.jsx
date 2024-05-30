f9d1827ebb1cfa479a293d7a449ee931a82b9030
HyunJong Kim <world2222222222@gmail.com>
2024-05-30 19:02:19 +0900

    FEAT: Implement Friends screen.
    Implmeented the design of the Friends screen. And now needs to build the
    logic of each button. Then it'll be fully implemented.

--- Beinus/src/component/home/Friends.jsx
+++ Beinus/src/component/home/Friends.jsx
@@ -1,6 +1,59 @@
+import { useEffect, useState } from "react"
+
 function Friends() {
-    
-    return(<h1>This is Friends page</h1>)
+
+    const [requests, setRequests] = useState([]);
+    const [requested, setRequested] = useState([]);
+    const [friends, setFriends] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/follows/get/" + localStorage.getItem("user_id"), {
+            method: 'GET', // HTTP method for getting data
+            headers: { 'Content-Type': 'application/json' }, // Set the content type
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setRequests(data.filter(item => item.state == "Requested" && item.user2 == localStorage.getItem("user_id")))
+            setRequested(data.filter(item => item.state == "Requested" && item.user1 == localStorage.getItem("user_id")))
+            setFriends(data.filter(item => item.state == "Friends"))
+        })
+    }, [])
+
+    return(
+        <div>
+            <h1>This is Friends page.</h1>
+            <h3>Follow Requests</h3>
+            {requests.reverse().map(item => (
+                <div key={item.id} className="profile-brief">
+                    <div className="profile-brief-image-and-name">
+                        <img src="/src/assets/Beinus Logo White 1x.png" width={"10%"} alt="Profile"/>
+                        <p className="profile-brief-name">{item.user1}</p>
+                    </div>
+                    <button className="profile-brief-button">Accept</button>
+                </div>
+            ))}
+            <h3>Follow Requested</h3>
+            {requested.reverse().map(item => (
+                <div key={item.id} className="profile-brief">
+                    <div className="profile-brief-image-and-name">
+                        <img src="/src/assets/Beinus Logo White 1x.png" width={"10%"} alt="Profile"/>
+                        <p className="profile-brief-name">{item.user2}</p>
+                    </div>
+                    <button className="profile-brief-button">Requested</button>
+                </div>
+            ))}
+            <h3>Friends</h3>
+            {friends.reverse().map(item => (
+                <div key={item.id} className="profile-brief">
+                    <div className="profile-brief-image-and-name">
+                        <img src="/src/assets/Beinus Logo White 1x.png" width={"10%"} alt="Profile"/>
+                        <p className="profile-brief-name">{item.user1 === localStorage.getItem('user_id') ? item.user2 : item.user1}</p>
+                    </div>
+                    <button className="profile-brief-button">Friends</button>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Friends
